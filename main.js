3203e26bfdcf3b9f9ef5b472a9993d54143f48f6
dialloel <dialloelhadji90@hotmail.fr>
2023-01-21 13:06:56 -0500

    ajout code java script et test du fonctionnement

---
+++ main.js
@@ -0,0 +1,25 @@
+ document.getElementById("form").onchange = function(){
+
+
+  var totalAmount = Number(document.getElementById("amount").value);
+
+  
+  var tipPercentage = document.getElementById("Tip").value;
+  
+  var tipAmount= totalAmount * (tipPercentage/100); 
+  var totalBill = totalAmount + tipAmount; 
+
+  document.getElementById("percentage-output").innerHTML = " " + tipPercentage + "%";
+  document.getElementById("totalbill").innerHTML = " " + totalBill; 
+
+
+
+}
+
+
+
+
+
+// const percentageOutput = document.getElementById("percentage-output");
+// var  tipAmount = document.getElementById("tipamount");
+// const totalBill = document.getElementById("totalbill");
